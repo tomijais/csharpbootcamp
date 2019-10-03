2f0f3a15ad3a8233e2a29e4fdc79c2cb576765da
tomijais <tomijais@gmail.com>
2019-10-03 20:27:18 -0300

    Programa Casa

---
+++ Programa Casa/Casa.cs
@@ -0,0 +1,13 @@
+namespace BobeFinder
+{
+    public class Casa
+    {
+        public double Area;
+        Puerta puerta;
+        public Casa (Puerta puerta, double area)
+        {
+        this.puerta = puerta;
+        this.Area = area;
+        }
+    }
+}
---
+++ Programa Casa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BobeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Puerta puerta = new Puerta();
+        puerta.Color = "Rojo";
+        SmallApartment depto = new SmallApartment(puerta);
+        Person persona = new Person();
+        persona.Nombre = "Alberto";
+        persona.casa = depto;
+
+        persona.ShowData();
+
+        }
+    }
+}
